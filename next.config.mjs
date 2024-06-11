f96fadd639c2e7e44180056609cdca16b52e82c2
omar-opt <133978379+omar-opt@users.noreply.github.com>
2024-06-11 23:03:23 +0100

    fix data

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 export default nextConfig;
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   /* config options here */
+// }
+
+// module.exports = nextConfig
